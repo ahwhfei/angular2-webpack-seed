7d19488bde3b407ab6764fdad1a1b0477df09d51
Hongfei Wu <hongfei.wu@citrix.com>
2017-01-12 17:30:11 +0800

    npm install html-webpack-plugin less-loader raw-loader --save-dev npm install reflect-metadata --save add node to typings.json add html.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: require('./app.component.html'),
+  styleUrls: [require('./app.component.less')]
 })
 export class AppComponent {
   title = 'app works!';
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
 
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/main.ts',
+    entry: path.resolve('src', 'main.ts'),
     module: {
         rules: [
             {
                 exclude: /node_modules/,
                 loader: 'ts-loader',
                 test: /\.ts$/,
+            },
+            {
+                exclude: /node_modules/,
+                loader: 'raw-loader',
+                test: /\.html$/,
+            },
+            {
+                exclude: /node_modules/,
+                loaders: ['raw-loader', 'less-loader'],
+                test: /\.less$/,
             }
         ]
     },
@@ -15,6 +27,11 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
     resolve: {
         extensions: ['.js', '.ts']
     }
