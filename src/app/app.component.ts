8da351d409187b9ad14944276a6f2495408448d5
Hongfei Wu <hongfei.wu@citrix.com>
2017-01-13 13:02:54 +0800

    fix style not work issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: require('./app.component.html'),
-  styleUrls: [require('./app.component.less')]
+  styles: [require('./app.component.less')]
 })
 export class AppComponent {
   title = 'app works!';
